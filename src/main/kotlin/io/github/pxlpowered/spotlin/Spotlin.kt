ee5475b6763796a7039506d8743e2b80df0af56f
SettingDust <settingdust@gmail.com>
2021-03-17 11:25:30 +0800

    :art: Format code & Lint log

--- src/main/kotlin/io/github/pxlpowered/spotlin/Spotlin.kt
+++ src/main/kotlin/io/github/pxlpowered/spotlin/Spotlin.kt
@@ -1,24 +1,6 @@
 /*
- * Copyright (c) 2017 PxlPowered
- * Copyright (c) Contributors
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
+ * This file is part of Spotlin, licensed under the MIT License (MIT). See the LICENSE file
+ * at the root of this project for more details.
  */
 
 package io.github.pxlpowered.spotlin
@@ -31,18 +13,23 @@ import org.spongepowered.api.plugin.Plugin
 import org.spongepowered.api.plugin.PluginContainer
 
 @Suppress("UNUSED_PARAMETER")
-@Plugin(id = "spotlin", name = "Spotlin", version = "0.2.0", description = "Provides the Kotlin runtime for other plugins.")
-class Spotlin @Inject constructor(val container: PluginContainer,
-                                    val logger: Logger) {
-
+@Plugin(
+    id = "spotlin",
+    name = "Spotlin",
+    version = "0.2.0",
+    description = "Provides the Kotlin runtime for other plugins."
+)
+class Spotlin @Inject constructor(
+    private val pluginContainer: PluginContainer,
+    private val logger: Logger
+) {
     @Listener
     fun onPreInit(e: GamePreInitializationEvent) {
-        logger.info("=====================")
-        logger.info("Using {} v{}, providing kotlin v{}",
-                container.name,
-                if (container.version.isPresent) container.version.get() else "unknown",
-                "1.3.0")
-        logger.info("=====================")
+        logger.info(
+            "Running {} v{}",
+            pluginContainer.name,
+            pluginContainer.version.orElse(null) ?: "unknown"
+        )
+        logger.info("        Kotlin v1.4.31")
     }
-
 }
